321d1aa932df15681138365c437fcd9ee904c8a5
Aakash tyagi <akashtyagi6065@gmail.com>
2025-10-11 22:27:44 +0530

    fix bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,19 @@
-    import { NextFunction, Request, Response } from "express";
-    import jwt  from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
-    export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-        const authHeader = req.headers['authorization'];
-        if (!authHeader) {
-            return res.status(401).json({ error: "Authorization header missing" });
-        }
-        const token = authHeader.split(' ')[1];
-        
-        try {
-            const decoded = jwt.verify(token, typeof process.env.JWT_SECRET);
-            // how to override the type of the express request object
-            //@ts-ignore
-            req.userId = decoded.userId;
-            next();
-        } catch (error) {
-            console.log(error)
-            return res.status(401).json({ error: "Invalid token" });
-        }
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).json({ error: "Authorization header missing" });
     }
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, typeof process.env.JWT_SECRET) as JwtPayload;
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        console.log(error)
+        return res.status(401).json({ error: "Invalid token" });
+    }
+}
